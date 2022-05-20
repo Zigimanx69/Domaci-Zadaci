86261d69ed6e1ee15d419a02762022ac121807ac
Igor Djokic <zigimanx@gmail.com>
2022-05-21 00:06:01 +0200

    Domaci 20.05.2022

--- d16_05_2022/src/main/java/d20_05_2022/FacebookPost.java
+++ d16_05_2022/src/main/java/d20_05_2022/FacebookPost.java
@@ -1,2 +1,41 @@
-package d20_05_2022;public class FacebookPost {
+package d20_05_2022;
+
+public class FacebookPost {
+
+    public String imePrezimeObjavljen;
+    public String imePrezimeNakomJeObjavljenPost;
+    public String tekstPoruke;
+    public int brojLajkova;
+    public int brojDeljenja;
+
+
+    public FacebookPost(){
+
+    }
+
+    public FacebookPost(String imePrezimeNakomJeObjavljenPost, String imePrezimeObjavljen, String tekstPoruke){
+      this.imePrezimeNakomJeObjavljenPost = imePrezimeNakomJeObjavljenPost;
+      this.imePrezimeObjavljen = imePrezimeObjavljen;
+      this.tekstPoruke = tekstPoruke;
+    }
+
+    public void like(){
+       this.brojLajkova= this.brojLajkova + 1;
+    }
+
+    public void dislike(){
+        if(this.brojLajkova >= 0){
+            this.brojLajkova = this.brojDeljenja - 1;
+        }
+    }
+
+    public void share(){
+        this.brojDeljenja = this.brojDeljenja + 1;
+    }
+
+    public void print(){
+        System.out.println(this.imePrezimeObjavljen + " >>> " + this.imePrezimeNakomJeObjavljenPost);
+        System.out.println(this.tekstPoruke);
+        System.out.println("Likes " + brojLajkova + " | " + "Shares " + brojDeljenja);
+    }
 }
--- d16_05_2022/src/main/java/d20_05_2022/MainFacebookPost.java
+++ d16_05_2022/src/main/java/d20_05_2022/MainFacebookPost.java
@@ -1,2 +1,23 @@
-package d20_05_2022;public class MainFacebookPost {
+package d20_05_2022;
+
+public class MainFacebookPost {
+    public static void main(String[] args) {
+        FacebookPost f = new FacebookPost();
+          f.imePrezimeObjavljen = "Igor Djokic";
+          f.imePrezimeNakomJeObjavljenPost = "Dragana Djokic";
+          f.tekstPoruke = "Ja sam legenda";
+          f.brojLajkova = 3;
+          f.brojDeljenja = 5;
+
+          f.like();
+          f.share();
+          f.print();
+
+        System.out.println("");
+
+          FacebookPost d = new FacebookPost("Ivana Djokic" , "Jasar Ahmedovski","Jednoj zeni za secanje dugo");
+          d.brojDeljenja = 2;
+          d.brojLajkova = 28;
+          d.print();
+    }
 }
